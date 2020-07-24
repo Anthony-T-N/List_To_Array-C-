e7b64bc9a7f8b864ff2f3b04a1f1854920cadf23
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-07-24 18:55:13 +0930

    Create example.cs

---
+++ example.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public static class placeholder
+{
+  public static int[] Capitals(string word)
+  {
+    List<int> index_list = new List<int>();
+		for (int i = 0; i <= word.Length - 1; i++)
+    {
+      if (word[i] == char.ToUpper(word[i]))
+      {
+        index_list.Add(i);
+      }
+    }
+    int[] index_array = new int[index_list.Count];
+    for (int i = 0; i <= index_list.Count - 1; i++)
+    {
+      index_array[i] = index_list[i];
+    }
+    return index_array;
+  }
+}
