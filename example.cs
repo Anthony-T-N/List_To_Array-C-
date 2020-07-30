b72420830433be55d3a015784c6e8714cbcde087
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-07-30 17:13:06 +0930

    Update example.cs

--- example.cs
+++ example.cs
@@ -23,3 +23,40 @@ public static class placeholder
     return index_array;
   }
 }
+
+// ===================================================================================================================
+
+using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+  public static int DuplicateCount(string str)
+  {
+    Dictionary<char, int> count_dict = new Dictionary<char, int>();
+    if (str.Length <= 0)
+    {
+      return 0;
+    }
+    for (int i = 0; i <= str.Length - 1; i++)
+    {
+      if (count_dict.ContainsKey(Char.ToLower(str[i])))
+      {
+        count_dict[Char.ToLower(str[i])] += 1;
+      }
+      else
+      {
+        count_dict.Add(Char.ToLower(str[i]), 1);
+      }
+    }
+    int total_duplicates = 0;
+    foreach (KeyValuePair<char, int> kvp in count_dict)
+    {
+      if (kvp.Value >= 2)
+      {
+        total_duplicates += 1;
+      }
+    }
+    return total_duplicates;
+  }
+}
